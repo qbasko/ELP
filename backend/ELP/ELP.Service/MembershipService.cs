e5c5eb0b9c692dfdd7a7091f078c92ff520a7fdc
Jakub Skoczeń jbsn@gft.com <jbsn@gft.com>
2017-03-09 15:54:50 +0100

    added createUser implementation and test for it

--- backend/ELP/ELP.Service/MembershipService.cs
+++ backend/ELP/ELP.Service/MembershipService.cs
@@ -3,48 +3,56 @@ using System.Collections.Generic;
 using System.Text;
 using ELP.Model.Entities;
 using ELP.Model;
+using ELP.Service.Exceptions;
 
 namespace ELP.Service
 {
     public class MembershipService : IMembershipService
     {
-        private readonly IContext _context;
         private readonly IUserService _userService;
         private readonly IEncryptionService _encryptionService;
+        private readonly IRoleService _roleService;
+        private readonly IUserRoleService _userRoleService;
 
-        public MembershipService(IUserService userService, IEncryptionService encryptionService)
+        public MembershipService(IUserService userService, IEncryptionService encryptionService, IRoleService roleService, IUserRoleService userRoleService)
         {
             _userService = userService;
             _encryptionService = encryptionService;
+            _roleService = roleService;
+            _userRoleService = userRoleService;
         }
 
         public User CreateUser(string username, string email, string password, ICollection<int> roles)
         {
-            
+
             User existingUser = _userService.GetUserByUsername(username);
-            if(existingUser!=null)
+            if (existingUser != null)
             {
-                throw new Exception("Username is already registered");
+                throw new UserAlreadyRegisteredException();
             }
 
             string passwordSalt = _encryptionService.CreateSalt();
 
             User user = new User()
             {
+                Id = 1,
                 Username = username,
                 Salt = passwordSalt,
                 Email = email,
                 IsLocked = false,
                 HashedPassword = _encryptionService.EncryptPassword(password, passwordSalt),
-                CreatedDate = DateTime.Now
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now
             };
 
             _userService.Create(user);
-            
-            //TODO Add roles
-            
-            return null;
-            
+
+            foreach (int roleId in roles)
+            {
+                AddUserToRole(user, roleId);
+            }
+
+            return user;
         }
 
         public User GetUser(int userId)
@@ -61,5 +69,26 @@ namespace ELP.Service
         {
             throw new NotImplementedException();
         }
+
+
+        private void AddUserToRole(User user, int roleId)
+        {
+            Role role = _roleService.GetRoleById(roleId);
+            if (role == null)
+            {
+                throw new Exception("Role does not exist!");
+            }
+
+            var userRole = new UserRole()
+            {
+                RoleId = role.Id,
+                UserId = user.Id,
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now
+            };
+
+            _userRoleService.Create(userRole);
+        }
+
     }
 }
