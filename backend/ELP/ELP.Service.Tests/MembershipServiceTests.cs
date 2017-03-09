aa115f325b1605ede0bac14393e56d3fe870f7f4
Jakub Skoczeń jbsn@gft.com <jbsn@gft.com>
2017-03-09 16:51:00 +0100

    added VerifyUser and test

--- backend/ELP/ELP.Service.Tests/MembershipServiceTests.cs
+++ backend/ELP/ELP.Service.Tests/MembershipServiceTests.cs
@@ -62,12 +62,78 @@ namespace ELP.Service.Tests
             IRoleService roleService = new RoleService(ctx.Object);
             IUserRoleService userRoleService = new UserRoleService(ctx.Object);
 
-            var encryptionService = new EncryptionService();
+            IEncryptionService encryptionService = new EncryptionService();
             IMembershipService service = new MembershipService(userService, encryptionService, roleService, userRoleService);
             User result = service.CreateUser("testUser1", "test@email.com", "password", new List<int>() { 1, 2 });
 
             usersMockDbSet.Verify(m => m.Add(It.IsAny<User>()), Times.Once);
-            userRolesMockDbSet.Verify(m => m.Add(It.IsAny<UserRole>()), Times.Exactly(2));            
+            userRolesMockDbSet.Verify(m => m.Add(It.IsAny<UserRole>()), Times.Exactly(2));
+        }
+
+        [Fact]
+        public void GetUserRolesTest()
+        {
+            var ctx = new Mock<IContext>();
+            List<Role> roles = new List<Role>();
+            roles.Add(new Role() { Id = 1, Name = "Role1" });
+            roles.Add(new Role() { Id = 2, Name = "Role2" });
+            
+            List<UserRole> userRoles = new List<UserRole>();
+            userRoles.Add(new UserRole() { Id = 1, RoleId = 1, UserId = 1, Role = roles[0] });
+            userRoles.Add(new UserRole() { Id = 2, RoleId = 2, UserId = 1, Role = roles[1] });
+
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Username = "testUser1", UserRoles = userRoles});
+
+            var rolesMockDbSet = ServiceTestsHelper.GetMockDbSet<Role>(roles);
+            var usersMockDbSet = ServiceTestsHelper.GetMockDbSet<User>(users);
+            var userRolesMockDbSet = ServiceTestsHelper.GetMockDbSet<UserRole>(userRoles);
+
+            ctx.Setup(c => c.Set<Role>()).Returns(rolesMockDbSet.Object);
+            ctx.Setup(c => c.Set<User>()).Returns(usersMockDbSet.Object);
+            ctx.Setup(c => c.Set<UserRole>()).Returns(userRolesMockDbSet.Object);
+
+            IUserService userService = new UserService(ctx.Object);
+            IRoleService roleService = new RoleService(ctx.Object);
+            IUserRoleService userRoleService = new UserRoleService(ctx.Object);
+            IEncryptionService encryptionService = new EncryptionService();
+            IMembershipService service = new MembershipService(userService, encryptionService, roleService, userRoleService);
+            List<Role> result = service.GetUserRoles("testUser1");
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void ValidateUserTest()
+        {
+            IEncryptionService encryptionService = new EncryptionService();
+            string userSalt = encryptionService.CreateSalt();
+            string userHash = encryptionService.EncryptPassword("password", userSalt);
+
+            var ctx = new Mock<IContext>();
+            List<Role> roles = new List<Role>();
+            roles.Add(new Role() { Id = 1, Name = "Role1" });
+            roles.Add(new Role() { Id = 2, Name = "Role2" });
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Username = "testUser1", Salt = userSalt, HashedPassword = userHash });
+            List<UserRole> userRoles = new List<UserRole>();
+            userRoles.Add(new UserRole() { Id = 1, RoleId = 1, UserId = 1 });
+            userRoles.Add(new UserRole() { Id = 2, RoleId = 2, UserId = 1 });
+
+            var rolesMockDbSet = ServiceTestsHelper.GetMockDbSet<Role>(roles);
+            var usersMockDbSet = ServiceTestsHelper.GetMockDbSet<User>(users);
+            var userRolesMockDbSet = ServiceTestsHelper.GetMockDbSet<UserRole>(userRoles);
+
+            ctx.Setup(c => c.Set<Role>()).Returns(rolesMockDbSet.Object);
+            ctx.Setup(c => c.Set<User>()).Returns(usersMockDbSet.Object);
+            ctx.Setup(c => c.Set<UserRole>()).Returns(userRolesMockDbSet.Object);
+
+            IUserService userService = new UserService(ctx.Object);
+            IRoleService roleService = new RoleService(ctx.Object);
+            IUserRoleService userRoleService = new UserRoleService(ctx.Object);
+
+            IMembershipService service = new MembershipService(userService, encryptionService, roleService, userRoleService);
+            var result = service.ValidateUser("testUser1", "password");
+            Assert.NotNull(result.Principal);
         }
     }
 }
